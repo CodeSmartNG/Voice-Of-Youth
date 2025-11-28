9ff4c163bdb9d3bc65afed13ebf65d993ffda304
Kabir Alkasim <codesmartng1@gmail.com>
2025-11-29 00:29:29 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['some-external-package']
-    }
-  },
   server: {
     port: 3000,
-    open: true // automatically open browser
+    open: true
   }
 })

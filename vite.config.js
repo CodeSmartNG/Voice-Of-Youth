50499503eee25ac0ad66e926490446e218a939c4
Kabir Alkasim <codesmartng1@gmail.com>
2025-11-29 21:08:35 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,12 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  
   build: {
     rollupOptions: {
-      // External packages that shouldn't be bundled
-      // These will need to be provided by the user's environment
-      external: [
-        'some-external-package',
-        'another-package'
-      ]
+      external: ['some-node-only-package'] // Add package/file here after verifying it's in package.json
     }
   }
 })

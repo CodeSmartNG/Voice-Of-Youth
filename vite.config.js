b059f292e28a44952b4eb5201e60de2716795384
Kabir Alkasim <codesmartng1@gmail.com>
2025-11-29 21:04:18 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,58 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   
-  // Build configuration
   build: {
     rollupOptions: {
+      // External packages that shouldn't be bundled
+      // These will need to be provided by the user's environment
       external: [
         'some-external-package',
-        'another-package',
-        // Add more external dependencies as needed
-        'lodash',
-        'axios',
-        'moment',
-        'jquery'
-      ],
-      
-      output: {
-        // Configure how external dependencies are handled
-        globals: {
-          'some-external-package': 'SomeExternalPackage',
-          'another-package': 'AnotherPackage',
-          'lodash': '_',
-          'axios': 'axios',
-          'moment': 'moment',
-          'jquery': '$'
-        }
-      }
-    },
-    
-    // Common build settings
-    minify: 'esbuild',
-    sourcemap: true,
-    outDir: 'dist'
-  },
-  
-  // Development server
-  server: {
-    port: 3000,
-    open: true
-  },
-  
-  // Preview server
-  preview: {
-    port: 4173
-  },
-  
-  // Path aliases
-  resolve: {
-    alias: {
-      '@': '/src',
-      '@components': '/src/components',
-      '@utils': '/src/utils'
+        'another-package'
+      ]
     }
   }
 })
